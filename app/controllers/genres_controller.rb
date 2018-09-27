288516b8c7b5f8ace3c395f46e77513083a0fa7f
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-27 22:05:11 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -17,6 +17,12 @@ class GenresController < ApplicationController
     @genre = Genre.find(params[:id])
   end 
 
+  def update 
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params(:name))
+    redirect_to genre_path(@genre)
+  end 
+
 
   private
 
