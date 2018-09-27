66f0fe4ffb6be6e018768a780f0181281de5ec11
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-27 22:23:31 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,4 +9,16 @@ class SongsController < ApplicationController
     @song = Song.new
   end 
 
+  def create 
+    @song = Song.new(song_params(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
+
+  end 
+
+  private
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end 
+
 end
