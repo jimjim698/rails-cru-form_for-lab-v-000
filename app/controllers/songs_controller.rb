e9758705f25a1ada3224fa3d369d88c35a9c8ff2
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-27 22:35:07 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
 
 
-  def index 
+  def index
   end 
 
   def show
@@ -27,7 +27,7 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
     @song.update(song_params(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
-  end 
+  end
 
   private
 
