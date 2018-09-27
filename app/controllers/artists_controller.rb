aa310965c33420d1ea00219aa322552e96e814ba
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-27 21:43:40 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -10,12 +10,12 @@ class ArtistsController < ApplicationController
 
   def create
     @artist = Artist.create(artist_params(:name, :bio))
-    redirect_to artist_path(@artist) 
+    redirect_to artist_path(@artist)
   end
 
-  def edit 
+  def edit
     @artist = Artist.find(params[:id])
-  end 
+  end
 
   private
 
